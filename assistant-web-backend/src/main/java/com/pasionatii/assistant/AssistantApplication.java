903996b3e83467be659f42f2e07608caa01961ae
Ike28 <mihaijrcorocaescu@gmail.com>
2023-06-04 16:13:21 +0300

    removed unnecessary annotation

--- assistant-web-backend/src/main/java/com/pasionatii/assistant/AssistantApplication.java
+++ assistant-web-backend/src/main/java/com/pasionatii/assistant/AssistantApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @ComponentScan("com.pasionatii.assistant.service")
 @ComponentScan("com.pasionatii.assistant.entity")
 @ComponentScan("com.pasionatii.assistant.controller")
-@EnableJpaRepositories(basePackages = "com.pasionatii.assistant")
 @CrossOrigin
 public class AssistantApplication {
 	public static void main(String[] args) {
