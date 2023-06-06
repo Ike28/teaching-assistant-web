4fbd9ed2427ebc62602057767de9a8260d72f3ee
Ike28 <mihaijrcorocaescu@gmail.com>
2023-06-06 23:43:05 +0300

    cors configuration

--- assistant-web-backend/src/main/java/com/pasionatii/assistant/AssistantApplication.java
+++ assistant-web-backend/src/main/java/com/pasionatii/assistant/AssistantApplication.java
@@ -3,8 +3,11 @@ package com.pasionatii.assistant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.pasionatii.assistant.entity")
@@ -16,5 +19,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class AssistantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssistantApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/assistant-javaconfig");
+			}
+		};
 	}
 }
