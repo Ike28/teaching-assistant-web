fbeec5642977145f5e3019fb0f58136b946ad922
andreea-bumbac <andreeabumbac2002@gmail.com>
2023-06-06 23:28:08 +0300

    controller

--- assistant-web-backend/src/main/java/com/pasionatii/assistant/controller/SubjectController.java
+++ assistant-web-backend/src/main/java/com/pasionatii/assistant/controller/SubjectController.java
@@ -1,2 +1,29 @@
-package com.pasionatii.assistant.controller;public class SubjectController {
+package com.pasionatii.assistant.controller;
+
+import com.pasionatii.assistant.repository.IQuizRepository;
+import com.pasionatii.assistant.repository.ISubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/assistant/subjects")
+public class SubjectController {
+    @Autowired
+    private ISubjectRepository subjectRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(subjectRepository.findAll(), HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "?subject-name={subjectName}", method = RequestMethod.GET)
+    public ResponseEntity<?> getSubjectsByName(@PathVariable String name) {
+        return new ResponseEntity<>(subjectRepository.findSubjectByName(name), HttpStatus.OK);
+    }
+
+
 }
--- assistant-web-backend/src/main/java/com/pasionatii/assistant/repository/ISubjectRepository.java
+++ assistant-web-backend/src/main/java/com/pasionatii/assistant/repository/ISubjectRepository.java
@@ -1,2 +1,10 @@
-package com.pasionatii.assistant.repository;public interface ISubjectRepository {
+package com.pasionatii.assistant.repository;
+
+import com.pasionatii.assistant.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface ISubjectRepository extends JpaRepository<Subject, Long> {
+    List<Subject> findAll();
+
+    Subject findSubjectByName(String name);
 }
